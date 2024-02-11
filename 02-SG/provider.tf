cb8e40fb60ea6a9c4fe54285ec6039f2d86bcbb3
Mohammed Abdulah Quraishi <mdabdulahquraish@gmail.com>
2024-02-11 09:17:57 +0300

    first jenkins

---
+++ 02-SG/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.31.0"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terra-roboshop"
+    key    = "sg"
+    region = "us-east-1"
+    dynamodb_table = "robo-table"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
